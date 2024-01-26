869964291885c0bac03eca8b49fcbd6910ad3536
Olivia <ikajiubachidiogo@gmail.com>
2024-01-26 17:29:47 +0100

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "Ikejiuba Olivia Chidiogo";
+const meta = {
+    title: "Ikejiuba Olivia Chidiogo",
+    description: "I’m Ikejiuba Oivia Chidiogo, a Java Developer ",
+};
+
+const introdata = {
+    title: "I’m Ikejiuba Chidiogo Olivia",
+    animated: {
+        first: "I love coding",
+        second: "I code Java/SpringBoot",
+        third: "I develop integrated and secure backend System",
+        fourth: "My Code orchestration breathe life into every project, I am the one for the job."
+    },
+    description: "",
+    your_img_url: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/365815778_5779209855513939_2821730805622339031_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=yi_ji-lcQhQAX_VDamY&_nc_ht=scontent-los2-1.xx&oh=00_AfDdSWL9hy29tN6SydgsSj1oQpwuVSrCl8BN8ivO58s5zw&oe=656D30E1",
+};
+
+const dataabout = {
+    title: "SUMMARY",
+    aboutme: " Passionate software engineer with a flair for innovation. I thrive on crafting efficient solutions and enjoy pushing the boundaries of technology. Dedicated to continuous learning and contributing my skills to impactful projects",
+};
+const worktimeline = [{
+        jobtitle: "REMOTE",
+        where: "",
+        date: "GLOBAL",
+    },
+    {
+        jobtitle: "HYBRID",
+        where: "",
+        date: "GLOBAL",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "JAVA",
+        value: 100,
+    },
+    {
+        name: "SPRING BOOT",
+        value: 100,
+    },
+    {
+        name: "MYSQL / POSTGRES",
+        value: 100,
+    },
+    {
+        name: "AWS/AZURE/GCP",
+        value: 100,
+    },
+    {
+        name: "GIT, CI/CD",
+        value: 100,
+    },
+    {
+        name: "KAFKA ",
+        value: 100,
+    },
+    {
+        name: "TDD",
+        value: 100,
+    },
+    {
+        name: "SWAGGER/POSTMAN",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Backend Developer",
+        description: "As an accomplished Backend Developer specializing in Java, Spring Boot, and microservices architecture, I excel in orchestrating the core functionality of digital ecosystems. With a precise blend of innovation, I design server-side solutions for high-performance applications.\n\nMy expertise in Java and Spring Boot allows me to create scalable backend solutions, including the development of RESTful APIs and the design of flexible microservices architectures. I adeptly manage relational databases like MySQL and PostgreSQL for data integrity and efficiency.\n\nCollaboration is integral to my approach, working seamlessly with DevOps teams to ensure continuous integration and deployment. I am proficient in troubleshooting bottlenecks and implementing new features with a focus on performance and scalability.\n\nCommitted to staying current with industry best practices, I stay informed about security standards and the latest advancements in backend technologies."
+    },
+
+    // {
+    //     title: "",
+    //     description: "",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407462985_6182605395174381_3220541341405342605_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_ohc=xG5yu6jrSfkAX8QRGp_&_nc_ht=scontent-los2-1.xx&oh=00_AfAVUqq_pRtXlKnmbWKEM3sc9sLrRQAhJKl4afa2n61dRA&oe=656E920C",
+        description: "Logistic App with Java as backend and Flutter for Mobile",
+        link: "https://play.google.com/store/apps/details?id=ng.ridex.user  ",
+    },
+    {
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/406651514_6182681931833394_4760269935540393507_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_ohc=5NAkroLIXzQAX8FC5bW&_nc_ht=scontent-los2-1.xx&oh=00_AfB4IpUFMaw-kzH0LQZJFm02YcqxS1NWX7xGuJzfLLI6tg&oe=656E855C",
+        description: "Books-ville app with Java and React",
+        link: "#",
+    },
+    {
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/399336547_6182680675166853_6560042604224829015_n.jpg?stp=dst-jpg_p180x540&_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_ohc=26BFx6kkFn0AX_l9z7F&_nc_ht=scontent-los2-1.xx&oh=00_AfCmM3HZB-C2MP-F0SJ3BXusDOmyR1NuHeolBHiCF0CD6w&oe=656DDA01",
+        description:  "Job-Hiring app with Java and React",
+        link: "#",
+
+    },
+    {
+        img: "        https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407373278_6182627215172199_105655842733005210_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_ohc=ifpR8GUPFEwAX-ik7lH&_nc_ht=scontent-los2-1.xx&oh=00_AfCAGBZUV3PquB2_nqPlJyduoyoS1AEFkXayjhq-RcevmQ&oe=656D47A1",
+        description: "Alpha Bank Fintech App with Java/React/flutter ",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ikejiubachidiogo@gmail.com",
+    YOUR_FONE: "+2347064540634",
+    description: "Thank you for taking your time to contact me, I will do well to respond as fast as possible. Please know that the number +2347064540634 is my whatsapp handle and I treat that too as official ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_77blnfx",
+    YOUR_TEMPLATE_ID: "template_uj0h72g",
+    YOUR_USER_ID: "rK8b_mSfYqNt-bVf4",
+};
+
+const socialprofils = {
+    github: "https://github.com/Divian29",
+    linkedin: "https://www.linkedin.com/in/olivia-ikejiuba-1167ba227/",
+
+
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
